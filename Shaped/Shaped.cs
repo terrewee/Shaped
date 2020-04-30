0b89a9646b470363aa3cb8c59db48a66aa4f4878
terrewee <43369518+terrewee@users.noreply.github.com>
2020-04-30 14:12:22 +0200

    Initial commit

---
+++ Shaped/Shaped.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Centipede {
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Shaped : GameEnvironment {
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent() {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            screen = new Point(470, 550);
+            ApplyResolutionSettings();
+
+            // TODO: use this.Content to load your game content here
+        }
+
+    }
+}
