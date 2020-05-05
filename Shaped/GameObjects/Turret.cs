a223e60a8e011a6ae7107dc92076334507f0afe9
terrewee <43369518+terrewee@users.noreply.github.com>
2020-05-05 16:20:33 +0200

    turret is made

---
+++ Shaped/GameObjects/Turret.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+namespace Shaped.GameObjects {
+    class Turret : SpriteGameObject {
+
+        private int timer = 0;
+        private bool idle = true;
+
+        public Turret(int layer = 0, string id = "", int sheetIndex = 0) : base("playing/spr_turret", layer, id, sheetIndex) {
+
+        }
+
+        public void Update(GameTime gameTime, GameObjectList bullets, GameObjectList enemies) {
+            base.Update(gameTime);
+            foreach (Enemy enemy in enemies.Children) {
+                if (Math.Abs(enemy.Position.Y - this.position.Y) < 50)
+                    idle = false;
+            }
+            if (!idle) {
+                if (timer == 60) {
+                    timer = 0;
+                    Bullet bullet = new Bullet();
+                    bullet.Poition = new Vector2(Position.X + sprite.Width, position.Y);
+                    bullets.Add(bullet);
+                }
+
+            }
+        }
+
+        public override void HandleInput(InputHelper inputHelper) {
+            base.HandleInput(inputHelper);
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch) {
+            base.Draw(gameTime, spriteBatch);
+        }
+    }
+}
